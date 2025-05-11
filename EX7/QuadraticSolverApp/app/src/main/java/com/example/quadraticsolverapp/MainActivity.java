e613f78d4dc36ff19c8bfb41508b14bd61dbaa59
ngocquang2509-pro <111073078+ngocquang2509-pro@users.noreply.github.com>
2025-05-11 16:26:15 +0700

    finish exercise 7 QuadraticSolverApp

---
+++ EX7/QuadraticSolverApp/app/src/main/java/com/example/quadraticsolverapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.quadraticsolverapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtA, edtB;
+    Button btnResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("LearnBundle");
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        btnResult = findViewById(R.id.btnResult);
+        btnResult.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            Bundle bundle = new Bundle();
+            int a = Integer.parseInt(edtA.getText().toString());
+            int b = Integer.parseInt(edtB.getText().toString());
+            bundle.putInt("SoA", a);
+            bundle.putInt("SoB", b);
+            intent.putExtra("mybackage",bundle);
+            startActivity(intent);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
